70a84e21d8894f71f4db556214f3cf265700b2ba
andev03 <nguyenhoangan03.work@gmail.com>
2025-06-17 13:23:14 +0700

    refector: refector application.yml to swagger cannot show actuator api

--- forum-service/src/main/java/com/kakan/forum_service/mapper/PostMapper.java
+++ forum-service/src/main/java/com/kakan/forum_service/mapper/PostMapper.java
@@ -1,15 +1,7 @@
 package com.kakan.forum_service.mapper;
 
-import com.kakan.forum_service.dto.response.PostResponseDto;
-import com.kakan.forum_service.pojo.Post;
 import org.mapstruct.ReportingPolicy;
-
-import java.util.List;
 
 @org.mapstruct.Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PostMapper {
-
-    PostResponseDto toDto(Post post);
-
-    List<PostResponseDto> toDtoList(List<Post> posts);
 }
