42f6c06329f51684f908eb07a0b48f245b18fedf
vankit123 <kietvkse182744@fpt.edu.vn>
2025-07-19 19:43:17 +0700

    pull

--- user-service/src/main/java/com/kakan/user_service/controller/OrderController.java
+++ user-service/src/main/java/com/kakan/user_service/controller/OrderController.java
@@ -5,18 +5,17 @@ import com.kakan.user_service.dto.response.ResponseDto;
 import com.kakan.user_service.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class OrderController {
     @Autowired
     private OrderService orderService;
-    @GetMapping("/updateRole")
-    public ResponseDto updateRole(OrderStatusRequest orderStatusRequest) {
+    @PostMapping("/updateRole")
+    public ResponseDto updateRole(@RequestBody OrderStatusRequest orderStatusRequest) {
         orderService.updateRole(orderStatusRequest);
+        System.out.println("Role updated for account ID: " + orderStatusRequest.getAccountId() + " with status: " + orderStatusRequest.getStatus());
         return new ResponseDto(HttpStatus.OK.value(), "Role updated successfully", null);
     }
 }
